9f7e0da4178a3eced9d4016c0d065dc947ccda0d
Hu Tao <hutao@cn.fujitsu.com>
2014-09-11 02:10:03 -0400

    runconfig.Config: add CpuLimit

--- runconfig/config.go
+++ runconfig/config.go
@@ -16,6 +16,7 @@ type Config struct {
 	MemorySwap      int64  // Total memory usage (memory + swap); set `-1' to disable swap
 	CpuShares       int64  // CPU shares (relative weight vs. other containers)
 	Cpuset          string // Cpuset 0-2, 0,1
+	CpuLimit        int64  // cpu time limit. This value sets cpu quota. Default cpu period is 100ms
 	AttachStdin     bool
 	AttachStdout    bool
 	AttachStderr    bool
@@ -43,6 +44,7 @@ func ContainerConfigFromJob(job *engine.Job) *Config {
 		MemorySwap:      job.GetenvInt64("MemorySwap"),
 		CpuShares:       job.GetenvInt64("CpuShares"),
 		Cpuset:          job.Getenv("Cpuset"),
+		CpuLimit:        job.GetenvInt64("CpuLimit"),
 		AttachStdin:     job.GetenvBool("AttachStdin"),
 		AttachStdout:    job.GetenvBool("AttachStdout"),
 		AttachStderr:    job.GetenvBool("AttachStderr"),
--- runconfig/parse.go
+++ runconfig/parse.go
@@ -75,6 +75,7 @@ func parseRun(cmd *flag.FlagSet, args []string, sysInfo *sysinfo.SysInfo) (*Conf
 		flWorkingDir      = cmd.String([]string{"w", "-workdir"}, "", "Working directory inside the container")
 		flCpuShares       = cmd.Int64([]string{"c", "-cpu-shares"}, 0, "CPU shares (relative weight)")
 		flCpuset          = cmd.String([]string{"-cpuset"}, "", "CPUs in which to allow execution (0-3, 0,1)")
+		flCpuLimit        = cmd.Int64([]string{"l", "-cpu-limit"}, 0, "CPU time limit")
 		flNetMode         = cmd.String([]string{"-net"}, "bridge", "Set the Network mode for the container\n'bridge': creates a new network stack for the container on the docker bridge\n'none': no networking for this container\n'container:<name|id>': reuses another container network stack\n'host': use the host network stack inside the container.  Note: the host mode gives the container full access to local system services such as D-bus and is therefore considered insecure.")
 		flRestartPolicy   = cmd.String([]string{"-restart"}, "", "Restart policy to apply when a container exits (no, on-failure[:max-retry], always)")
 		// For documentation purpose
@@ -270,6 +271,7 @@ func parseRun(cmd *flag.FlagSet, args []string, sysInfo *sysinfo.SysInfo) (*Conf
 		Memory:          flMemory,
 		CpuShares:       *flCpuShares,
 		Cpuset:          *flCpuset,
+		CpuLimit:        *flCpuLimit,
 		AttachStdin:     flAttach.Get("stdin"),
 		AttachStdout:    flAttach.Get("stdout"),
 		AttachStderr:    flAttach.Get("stderr"),
